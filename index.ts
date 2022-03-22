e7bb3896fd052a23e8e39c1867d1cc2af87cf50e
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-22 11:02:41 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { Observable, interval } from 'rxjs';
 import { ajax, AjaxResponse } from 'rxjs/ajax';
-const apiKey = '0475be3a1967b1b49dfc02c8128001a';
+const apiKey = 'd0475be3a1967b1b49dfc02c8128001a';
 const URL =
   'https://api.openweathermap.org/data/2.5/weather?APPID=' +
   apiKey +
   '&units=metric&q=';
-var city = 'Pisa';
+var city = 'Viterbo';
 const obs = ajax(URL + city);
 //obs.subscribe({
 //  next: (res: AjaxResponse) => res.response.main.temp,
@@ -17,7 +17,7 @@ tick.subscribe({
   next: (x) => {
     obs.subscribe({
       next: (res: AjaxResponse) => console.log(res.response.main.temp),
-      error: (err: Error) => console.error(err.message),
+      error: (err: Error) => console.error("Sorry: ",err.message),
     });
   },
 });
@@ -27,7 +27,7 @@ tick.subscribe({
       next: (res: AjaxResponse) =>
         document.getElementById('output').innerHTML +=
           res.response.main.temp + '<br>',
-      error: (err: Error) => console.error(err.message),
+      error: (err: Error) => console.error("Uela! " + err.message),
     });
   },
 });
