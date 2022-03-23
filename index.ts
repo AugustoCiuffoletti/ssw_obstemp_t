21c7e6ce0481f643911d1f7132fee194155fc602
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-23 11:15:51 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Observable, interval } from 'rxjs';
 
 const apiKey = 'd0475be3a1967b1b49dfc02c8128001a';
 const URL =
-  'https://api.openweathermap.org/data/2.5/weather?APPID='+apiKey +'&units=metric&q=';
+  'https://api.openweathermap.org/data/2.5/weather?APPID=' + apiKey + '&units=metric&q=';
 var city = 'Pisa';
 const tick: Observable<number> = interval(10000);
 const temp: Observable<any> = new Observable((subscriber) =>
@@ -12,7 +12,7 @@ const temp: Observable<any> = new Observable((subscriber) =>
         .then((response) => response.json())
         .then((data) => subscriber.next(data.main.temp));
     },
-    error: (err) => console.log ("C'è un errore", err)
+    error: (err) => console.log('Errore: ', err),
   })
 );
 // Due subscriber
