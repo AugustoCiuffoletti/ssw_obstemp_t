9e5920ccd3830f1685ce71f6eb0319caa08cd663
AugustoCiuffoletti <augusto.ciuffoletti@unipi.it>
2023-03-22 13:52:56 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,24 +1,26 @@
-import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
+import { Observable, interval } from 'rxjs';
 const apiKey = 'd0475be3a1967b1b49dfc02c8128001a';
-const URL = 'https://api.openweathermap.org/data/2.5/weather?APPID=' + apiKey +  '&units=metric&q=';
+const URL =
+  'https://api.openweathermap.org/data/2.5/weather?APPID=' +
+  apiKey +
+  '&units=metric&q=';
 var city = 'Pisa';
-const request: AjaxRequest = {
-  url: URL + city,
-  crossDomain: true,
-};
-const temp: Observable<AjaxResponse<any>> = ajax(request);
 const tick: Observable<number> = interval(10000);
-const obs: Observable<any> = new Observable((subscriber) =>
+const temp: Observable<any> = new Observable((subscriber) =>
   tick.subscribe({
     next: () => {
-      temp.subscribe({
-        next: (res: AjaxResponse<any>) =>
-          subscriber.next(res.response.main.temp),
-        error: (err: AjaxError) => console.error('Error: ', err.request),
-      });
+      fetch(URL + city)
+        .then((response) => response.json())
+        .then((data) => subscriber.next(data.main.temp));
     },
     error: (err) => console.log('Errore: ', err),
   })
 );
-obs.subscribe({ next: (x) => console.log(x) });
-obs.subscribe({ next: (x) => (document.getElementById('output').innerHTML += x + '<br>') });
+temp.subscribe({
+  // Primo subscriber
+  next: (x) => console.log(x),
+});
+temp.subscribe({
+  // Secondo subscriber
+  next: (x) => (document.getElementById('output').innerHTML += x + '<br>'),
+});
