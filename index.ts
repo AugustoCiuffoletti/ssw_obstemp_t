81b476a94449fbcdaacca6f363eceab0bcb680f9
AugustoCiuffoletti <augusto.ciuffoletti@unipi.it>
2023-03-22 13:35:44 +0100

    compattato

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { Observable, interval } from 'rxjs';
 import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
 const apiKey = 'd0475be3a1967b1b49dfc02c8128001a';
-const URL = 'https://api.openweathermap.org/data/2.5/weather?APPID=' + apiKey +  '&units=metric&q=Pisa';
+const URL = 
+  'https://api.openweathermap.org/data/2.5/weather?APPID=' + apiKey +  '&units=metric&q=Pisa';
 const request: AjaxRequest = { url: URL, crossDomain: true, };
 const temp: Observable<AjaxResponse<any>> = ajax(request);
 const tick: Observable<number> = interval(10000);
